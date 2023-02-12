77929d9318dc7fcc301bfaa927610ad45ed7bf95
PPlain <124816397+PPlain@users.noreply.github.com>
2023-02-12 20:12:31 +0000

    Create ChatWelcomer.lua

---
+++ ChatWelcomer.lua
@@ -0,0 +1,7 @@
+game.StarterGui:SetCore("ChatMakeSystemMessage", {
+    Text = "Welcome to my game!";
+    Color = Color3.new(255, 221, 0);
+    Font = Enum.Font.SourceSansBold;
+    FontSize = Enum.FontSize.Size24;
+
+-- CREATE A LOCAL SCRIPT IN StarterCharacterScripts
